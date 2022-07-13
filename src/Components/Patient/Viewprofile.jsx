35af8fc412506dfcde7bcedd56d0bbb4bbda61dc
Saytzeff-D <davidatanda2020@gmail.com>
2022-07-13 21:23:28 +0100

    I updated patieent profile

--- src/Components/Patient/Viewprofile.jsx
+++ src/Components/Patient/Viewprofile.jsx
@@ -1,9 +1,6 @@
 import axios from 'axios'
-import { useFormik } from 'formik'
-import React,{useState} from 'react'
-import { useEffect } from 'react'
+import React,{ useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import * as Yup from 'yup'
 
 
 const ViewProfile=()=>{
