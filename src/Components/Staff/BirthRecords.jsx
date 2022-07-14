48e1100ea22e50f03d5d6dc383c47628eb794edd
Saytzeff-D <davidatanda2020@gmail.com>
2022-07-14 15:37:03 +0100

    now adding birth records, remain axios connection

--- src/Components/Staff/BirthRecords.jsx
+++ src/Components/Staff/BirthRecords.jsx
@@ -6,28 +6,53 @@ import { useSelector } from 'react-redux'
 const BirthRecords = (props)=>{
     const [error, setError] = useState('')
     const birthTray = useSelector(state=>state.RecordsReducer.birthRecords)
+    const patientTray = useSelector(state=>state.PatientReducer.patientTray)
+    const [motherName, setMotherName] = useState('')
     const formik = useFormik({
         initialValues: {
             childName: '',
             gender: '',
             weight: '',
             birthDate: '',
+            address: '',
+            motherHealthId: '',
             motherName: '',
             fatherName: '',
-            report: '',
+            report: '', 
             childPhoto: ''
         },
         onSubmit: (values)=>{
             console.log(values)
         }
     })
+    const clickFileInput = ()=>{
+        document.getElementById('childPhoto').click()
+    }
+    const pickFile = (file)=>{
+        const reader = new FileReader()
+        reader.readAsDataURL(file)
+        reader.onload = ()=>{
+            formik.values.childPhoto = reader.result
+        }
+    }
+    const getMotherName = (motherId)=>{
+        const mother = patientTray.find((patient, i)=>(patient.healthId === motherId))
+        if(mother === undefined){
+            setMotherName('Record not found...')
+        }else{
+            formik.values.motherHealthId = motherId
+            formik.values.motherName = mother.fullName
+            setMotherName(mother.fullName)
+        }
+
+    }
     return(
         <>
             <div className='container-fluid p-3'>
                 <div className='row'>
                     <div className='col-12 bg-white border'>
                         <div className='d-flex justify-content-between border-bottom py-2'>
-                            <p>Birth Records</p>
+                            <p className='h6'>Birth Records</p>
                             <button className='btn btn-primary' data-target='#birthModal' data-toggle='modal'><FontAwesomeIcon icon='plus' /> Add Birth Record</button>
                         </div>
                         <div className='col-lg-4 col-md-6 col-sm-8 my-2'>
@@ -46,7 +71,7 @@ const BirthRecords = (props)=>{
                                 <table className='table table-light table-striped table-responsive'>
                                     <thead>
                                         <tr>
-                                            <th>Ref No</th>
+                                            <th>Records Id</th>
                                             <th>Child Name</th>
                                             <th>Gender</th>
                                             <th>Birth Date</th>
@@ -59,6 +84,7 @@ const BirthRecords = (props)=>{
                                         {
                                             birthTray.map((item, index)=>(
                                                 <tr>
+                                                    <td> {item.recordsId} </td>
                                                     <td> {item.childName} </td>
                                                     <td> {item.gender} </td>
                                                     <td> {item.birthDate} </td>
@@ -113,25 +139,25 @@ const BirthRecords = (props)=>{
                                     <div className='form-row'>
                                         <div className='form-group col-sm-4'>
                                             <label>Birth Date</label>
-                                            <input type='date' name='birthDate' className='form-control' />
+                                            <input type='date' name='birthDate' className='form-control' onChange={formik.handleChange} max={new Date().toISOString().split('T')[0]} />
                                         </div>
                                         <div className='form-group col-sm-8'>
                                             <label>Address</label>
-                                            <input type='text' name='address' className='form-control' />
+                                            <input type='text' name='address' className='form-control' onChange={formik.handleChange} />
                                         </div>
                                     </div>
                                     <div className='form-row'>
                                         <div className='form-group col-sm-4'>
-                                            <label>Mother(Patient) Id</label>
-                                            <input name='' className='form-control' />
+                                            <label>Mother's Health Id</label>
+                                            <input name='' className='form-control' onChange={(e)=>getMotherName(e.target.value)} />
                                         </div>
                                         <div className='form-group col-sm-4'>
                                             <label>Mother's Name</label>
-                                            <input name='motherName' className='form-control' disabled />
+                                            <input name='motherName' onChange={formik.handleChange} value={motherName} className='form-control' style={{color: motherName === 'Record not found...' ? '#ff0000' : ''}} disabled />
                                         </div>
                                         <div className='form-group col-sm-4'>
                                             <label>Father's Name</label>
-                                            <input name='fatherName' className='form-control' />
+                                            <input name='fatherName' className='form-control' onChange={formik.handleChange} />
                                         </div>
                                     </div>
                                     <div className='form-row'>
@@ -141,8 +167,8 @@ const BirthRecords = (props)=>{
                                         </div>
                                         <div className='form-group col-md-4'>
                                             <label>Child's Photo</label>
-                                            <input type='file' name='childPhoto' className='form-control d-none' onChange={formik.handleChange} />
-                                            <button type='pickFile' className='btn btn-primary btn-block font-weight-bold'><span><i className='fa fa-upload'></i></span> Click to drop a file</button>
+                                            <input onChange={(e)=>pickFile(e.target.files[0])} type='file' name='childPhoto' id='childPhoto' className='form-control d-none' />
+                                            <div onClick={clickFileInput} className='btn btn-primary btn-block font-weight-bold'><span><i className='fa fa-upload'></i></span> Click to drop a file</div>
                                         </div>
                                     </div>
                                     <button type='submit' className='btn btn-primary btn-block font-weight-bold'>Save Records</button>
