1934184a9c8506b653bdb7dc044c726c9b776d22
Saytzeff-D <davidatanda2020@gmail.com>
2022-07-19 17:35:44 +0100

    I fetched all drugs from sidebar

--- src/Components/Staff/StaffSideBar.jsx
+++ src/Components/Staff/StaffSideBar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import axios from 'axios'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { getAllPatient, setStaff } from '../../actions';
+import { allMedicines, getAllPatient, setStaff } from '../../actions';
 import LogoutMessage from '../Container/LogoutMessage'
 
 const StaffSideBar = ()=>{
@@ -28,6 +28,7 @@ const StaffSideBar = ()=>{
             console.log(res.data.staffDetails)
           dispatch(setStaff(res.data.staffDetails))
           dispatch(getAllPatient(url))
+          dispatch(allMedicines(url))
           setIsLoading(false)
         }
         else{
