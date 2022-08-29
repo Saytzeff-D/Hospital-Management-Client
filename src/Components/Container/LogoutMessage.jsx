572d155a3c15c3e2d1c46a1445d3eb1be27bc1ba
Saytzeff-D <davidatanda2020@gmail.com>
2022-08-29 17:40:01 +0100

    removed the onNoClick

--- src/Components/Container/LogoutMessage.jsx
+++ src/Components/Container/LogoutMessage.jsx
@@ -12,19 +12,13 @@ function LogoutMessage(props) {
             window.location = '/views/staffLogin'
         }
     }
-    const onNoClick = ()=>{
-        if(route === 'patient'){
-            window.location = '/patient/dashboard'
-        }else{
-            window.location = '/staff/dashboard'
-        }
-    }
+    
     return (
         <div>
             <p>Are you sure you want to leave?</p>
             <div className='d-flex'>
                 <button onClick={onYesClick} className='btn btn-danger m-1'>Yes, I'm sure</button>
-                <button onClick={onNoClick} className='btn btn-success m-1'>No, I'm not</button>
+                <button data-dismiss='modal' className='btn btn-success m-1'>No, I'm not</button>
             </div>
         </div>
     );
