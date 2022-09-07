b46d79e342e45e3bbc17ba15b1f549ed97e16f0d
AyodejiAfolabi <joelayodeji2019@gmail.com>
2022-09-07 14:54:29 +0100

    wildcard route  added

--- src/Components/Container/WildCard.jsx
+++ src/Components/Container/WildCard.jsx
@@ -4,7 +4,7 @@ const WildCard = () => {
     return (
         <div>
             <div className='not-found-page p-5'>
-                <p className='display-1 font-weight-bold text-warning'>Page not found</p>
+                <p className='display-1 font-weight-bold text-warning'>Page not found.</p>
                 <a href='/' className='text-white font-weight-bold h6' ><i className='fa fa-arrow-left'></i> Go back home </a>
             </div>
         </div>
