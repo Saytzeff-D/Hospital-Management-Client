ca6c553e0e547b47f1477ca1a8a3bf8af71f8dbf
Saytzeff-D <davidatanda2020@gmail.com>
2022-09-27 07:21:05 +0100

    active className are now working

--- src/Components/Patient/PatientSideBar.jsx
+++ src/Components/Patient/PatientSideBar.jsx
@@ -58,13 +58,13 @@ const PatientSideBar = ()=>{
     return(
         <div>
             {/* Top Container */}
-            <div className="w3-bar w3-top w3-blue w3-large" style={{zIndex: 4}}>
+            <div className="w3-bar w3-top w3-blue w3-large border-bottom" style={{zIndex: 4}}>
                 <button className="w3-bar-item w3-button w3-hide-large w3-hover-none w3-hover-text-light-grey" onClick={w3_open}><i className="fa fa-bars"></i></button>
                 <span className="w3-bar-item w3-left font-weight-bold">Hospital Management System</span>
                 <span data-toggle='modal' data-target='#logoutModal' className='w3-right p-2 w3-hide-small font-weight-bold' onClick={Logout} style={{cursor: 'pointer'}}>Logout <i className='fa fa-power-off'></i></span>
             </div>
             {/* SideNav Menu */}
-            <nav className="w3-sidebar w3-collapse w3-white w3-animate-left" style={mySidebarStyle}>
+            <nav className="w3-sidebar w3-collapse w3-blue w3-animate-left" style={mySidebarStyle}>
                 <br />
                 <div className="w3-container w3-row">
                     {
@@ -92,11 +92,11 @@ const PatientSideBar = ()=>{
                     <h5>Dashboard</h5>
                 </div>
                 <div className="w3-bar-block">
-                    <NavLink style={{textDecoration: 'none'}} to='/patient/dashboard' activeClassName='w3-blue' className="w3-bar-item w3-button w3-padding"><i className="fa fa-users fa-fw"></i>  Overview</NavLink> 
-                    <NavLink style={{textDecoration: 'none'}} to='/patient/appointment' activeClassName='w3-blue' className="w3-bar-item w3-button w3-padding"><i className="fa fa-calendar fa-fw"></i>  My Appointments</NavLink> 
-                    <NavLink style={{textDecoration: 'none'}} to='/patient/pharmacy' activeClassName='w3-blue' className="w3-bar-item w3-button w3-padding"><i className="fa fa-users fa-fw"></i>  Pharmacy</NavLink> 
-                    <NavLink style={{textDecoration: 'none'}} to='/patient/ambulance' activeClassName='w3-blue' className="w3-bar-item w3-button w3-padding"><i className="fa fa-ambulance fa-fw"></i>  Ambulance</NavLink> 
-                    <NavLink style={{textDecoration: 'none'}} to='/patient/liveChat' activeClassName='w3-blue' className="w3-bar-item w3-button w3-padding"><i className="fa fa-comment-o fa-fw"></i>  Live Chat</NavLink>
+                    <NavLink exact style={{textDecoration: 'none'}} to='/patient/dashboard' className={({isActive})=>( isActive ? 'w3-padding w3-bar-item bg-white text-primary' : 'w3-bar-item w3-button w3-padding' )}><i className="fa fa-users fa-fw"></i>  Overview</NavLink> 
+                    <NavLink style={{textDecoration: 'none'}} to='/patient/appointment' className={({isActive})=>( isActive ? 'w3-padding w3-bar-item bg-white text-primary' : 'w3-bar-item w3-button w3-padding' )}><i className="fa fa-calendar fa-fw"></i>  My Appointments</NavLink> 
+                    <NavLink style={{textDecoration: 'none'}} to='/patient/pharmacy' className={({isActive})=>( isActive ? 'w3-padding w3-bar-item bg-white text-primary' : 'w3-bar-item w3-button w3-padding' )}><i className="fa fa-users fa-fw"></i>  Pharmacy</NavLink> 
+                    <NavLink style={{textDecoration: 'none'}} to='/patient/ambulance' className={({isActive})=>( isActive ? 'w3-padding w3-bar-item bg-white text-primary' : 'w3-bar-item w3-button w3-padding' )}><i className="fa fa-ambulance fa-fw"></i>  Ambulance</NavLink> 
+                    <NavLink style={{textDecoration: 'none'}} to='/patient/liveChat' className={({isActive})=>( isActive ? 'w3-padding w3-bar-item bg-white text-primary' : 'w3-bar-item w3-button w3-padding' )}><i className="fa fa-comment-o fa-fw"></i>  Live Chat</NavLink>
                     <p style={{textDecoration: 'none'}} onClick={Logout} data-toggle='modal' data-target='#logoutModal' className="w3-bar-item w3-button w3-padding w3-hide-large"><i className="fa fa-power-off fa-fw"></i>  Logout</p> 
                 </div>
             </nav>
@@ -105,7 +105,7 @@ const PatientSideBar = ()=>{
             <div className="w3-overlay w3-hide-large w3-animate-opacity" style={overlayBgStyle} onClick={w3_open} title="close side menu"></div>
 
             {/* Page Content */}
-            <div className="w3-main" style={{marginLeft: '300px', marginTop: '43px'}}>
+            <div className="w3-main text-white" style={{marginLeft: '300px', marginTop: '43px'}}>
                 {
                     loading 
                     ?
